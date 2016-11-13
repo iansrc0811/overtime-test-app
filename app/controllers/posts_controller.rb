80126684834783619606415cce60af78aed9623c
ian yeh <iansrc0811@hotmail.com>
2016-11-13 22:27:42 +0800

    add edit and update in posts_controller.rb and view, create 'click_line(link to id)' function of capybara, it is so useful that we can link to certain link by id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit, :update]
   def index
     @posts = Post.all #@post's'
   end
@@ -19,6 +19,16 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Your post was created successfully'
+    else
+      reder :edit
+    end
+  end
   def show
     
   end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,13 +1,15 @@
 #factory girl
 #命名用model name的複數
 FactoryGirl.define do
+ # 使用FactoryGirl.create(:post) 可在測試中建立一個post
   factory :post do
     date Date.today
     rationale "Some Rationale"
     user
   end
 
-  factory :second_post, class: "Post" do
+  factory :second_post, class: "Post" do 
+  #使用FactoryGirl.create(:second_post) 可在測試中建立另一個post
     date Date.yesterday
     rationale "Some more content"
     user
