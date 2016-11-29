e22ba13b138b4f17e60af98cd924ec08d6055fe8
ian yeh <iansrc0811@hotmail.com>
2016-11-29 23:49:29 +0800

    block non admin to edit status of posts

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -9,15 +9,22 @@ describe 'navigate' do
   describe 'edit' do
     before do
       @post = FactoryGirl.create(:post)
+      visit edit_post_path(@post)
     end
 
-    it 'has a status that can be edited on the form' do #測試 post status(radio bottum )
-      visit edit_post_path(@post)
-
+    it 'has a status that can be edited on the form by an adim' do #測試 post status(radio bottum )
       choose('post_status_approved')
       click_on "Save"
 
       expect(@post.reload.status).to eq('approved') # database有存取要記得加上'reload'不然會一直錯誤
+    end
+
+    it 'can not be edited by a non admin' do
+      logout(:user)
+      @user = FactoryGirl.create(:user)
+      login_as(@user, :scope => :user)
+      visit edit_post_path(@post)
+      expect(page).to_not have_content('Approved') # 頁面不要出現'Approved' (radio bottun的labe之一 ，) 也就是不要產生radio bottun就對了
     end
   end
 end
